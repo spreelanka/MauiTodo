d3f36deaba5a787df30caab434dc7771e6e6ce5d
Jacob Hudson <jacob@portablefoundations.com>
2023-08-07 22:23:25 -0400

    init

---
+++ MauiTodo/MauiTodo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiTodo;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ MauiTodo/MauiTodo/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MauiTodo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
