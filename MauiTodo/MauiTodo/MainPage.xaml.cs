630d35cd3023e2ff8f6bc87902478d5a6181de36
Jacob Hudson <jacob@portablefoundations.com>
2023-08-08 20:32:07 -0400

    add basic persistence we can improve and secure later

--- MauiTodo/MauiTodo/MainPage.xaml.cs
+++ MauiTodo/MauiTodo/MainPage.xaml.cs
@@ -1,24 +1,42 @@
 ﻿namespace MauiTodo;
 
+using GoogleGson;
+using MauiTodo.Models;
+using MauiTodo.Services;
+using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
+
 public partial class MainPage : ContentPage
 {
 	int count = 0;
+    IDataProvider dataProvider;
 
-	public MainPage()
+    public MainPage(IDataProvider dataProvider)
 	{
+        this.dataProvider = dataProvider;
 		InitializeComponent();
+		Task.Run(async () =>
+		{
+            var d = await dataProvider.Get<Data>(0);
+            count = d.Count.Value;
+            updateText();
+        });
 	}
+    private void updateText()
+    {
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
+
+        SemanticScreenReader.Announce(CounterBtn.Text);
+    }
 
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+        dataProvider.Put<Data>(new Data { Count = new Count { Value = count } });
+        updateText();
 	}
 }
 
---
+++ MauiTodo/MauiTodo/Services/IDataProvider.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace MauiTodo.Services
+{
+	public interface IDataProvider
+	{
+        Task<T> Get<T>(int id);
+        Task Put<T>(T data);
+	}
+}
+
